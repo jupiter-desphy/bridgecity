b30884960c8c4cd1c5556562052502a293aaab67
Jupiter Desphy <jupiterdesphy7@gmail.com>
2023-04-09 20:56:56 -0700

    added pages and page index

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import { About } from "./About.page";
+import { Contact } from "./Contact.page";
+import { Reviews } from "./Reviews.page";
+import { Landing } from "./Landing.page";
+
+export { Landing, About, Contact, Reviews };
